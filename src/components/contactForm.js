e3a68ce5b2ffb552dbc24cc56b6d0c6905a2c4f3
Anju Cheran <cheran.anju@gmail.com>
2021-01-05 21:23:16 +0530

    handle form using fetch

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -5,32 +5,67 @@ class ContactForm extends React.Component{
     constructor(props) {
         super(props)
         this.state = {
-            checkedB: false
+            botField: '',
+            name: '',
+            email: '',
+            company: '',
+            consent: false
         }
     }
 
-    handleChange = () => {
+    handleName = (evt) => {
         this.setState({
-            checkedB: !this.state.consent
+            name: evt.target.value
         })
+    }
+
+    handleEmail = (evt) => {
+        this.setState({
+            email: evt.target.value
+        })
+    }
+
+    handleCompany = (evt) => {
+        this.setState({
+            company: evt.target.value
+        })
+    }
+
+    handleConsent = () => {
+        this.setState({
+            consent: !this.state.consent
+        })
+    }
+
+    encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&")
     }
 
     handleSubmit = (evt) => {
         evt.preventDefault()
-        console.log(evt)
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: this.encode({
+              "form-name": evt.target.getAttribute("name"),
+              ...this.state
+            })
+          }).then(() => console.log("/thank-you/")).catch(error => alert(error))
     }
     
     render() {
         return (
             <Container>
-            <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact" onSubmit={this.handleSubmit}>
+            <form netlify-honeypot="bot-field" data-netlify="true" name="contact" onSubmit={this.handleSubmit}>
                 <input type="hidden" name="bot-field" />
                 <input type="hidden" name="form-name" value="contact" />
                 <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <TextField fullWidth label="First Name" name="name" size="small" variant="outlined" required />
+                            <TextField fullWidth label="First Name" name="name" size="small" variant="outlined" required onChange={this.handleName} />
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
@@ -41,17 +76,18 @@ class ContactForm extends React.Component{
                                 type="email"
                                 variant="outlined"
                                 required
+                                onChange={this.handleEmail}
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth label="Company Name" name="company" size="small" variant="outlined" />
+                            <TextField fullWidth label="Company Name" name="company" size="small" variant="outlined" onChange={this.handleCompany} />
                         </Grid>
                         <Grid item xs={12}>
                         <FormControlLabel
                             control={
                             <Checkbox
                                 checked={this.state.consent}
-                                onChange={this.handleChange}
+                                onChange={this.handleConsent}
                                 name="consent"
                                 color="primary"
                             />
